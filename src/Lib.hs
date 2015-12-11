910932e6d04d9337fbc1fb575ff6f20ccbcb9ac2
Pascal Hartig <phartig@rdrei.net>
2015-12-11 11:04:55 +0000

    Some parsing and failing tests

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,28 +2,47 @@
 
 module Lib
     ( server
+    , readDescriptions
     ) where
 
-import qualified Web.Scotty as S
-import qualified Network.Wreq as W
-import qualified Data.Text.Lazy as T
+import qualified Data.ByteString.Lazy    as BS
+import qualified Data.Text.Lazy          as T
 import qualified Data.Text.Lazy.Encoding as TE
-import qualified Data.ByteString.Lazy as BS
+import qualified Network.Wreq            as W
+import qualified Web.Scotty              as S
 
-import Control.Monad.IO.Class (liftIO)
-import Control.Lens
+import           Control.Arrow           ((>>>))
+import           Control.Monad.IO.Class  (liftIO)
+
+import           Control.Lens            hiding (deep)
+import           Text.XML.HXT.Core
 
 feedUrl :: String
 feedUrl = "http://feeds.soundcloud.com/users/soundcloud:users:189413584/sounds.rss"
 
+type Description = String
+
 server :: IO ()
-server = do
-  S.scotty 3000 $ do
+server =
+  S.scotty 3000 $
     S.get "/feed.rss" $ do
       res <- liftIO $ fetchFeed feedUrl
-      S.text res
+      let doc = readString [withWarnings yes] $ T.unpack res
+      descs <- liftIO $ readDescriptions doc
+      S.text $ T.pack $ show descs
 
 fetchFeed :: String -> IO T.Text
 fetchFeed url = do
   r <- W.get url
   return $ TE.decodeUtf8 $ r ^. W.responseBody
+
+readDescriptions :: IOSLA (XIOState ()) XmlTree XmlTree -> IO [Description]
+readDescriptions doc =
+  runX $ doc >>> selectDescriptions >>> parseDescription
+
+  where
+    selectDescriptions = getChildren >>> isElem >>> hasName "itunes:summary"
+    parseDescription = atTag "itunes:summary" >>> getText
+
+atTag :: ArrowXml a => String -> a XmlTree XmlTree
+atTag tag = deep (isElem >>> hasName tag)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,37 @@
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE NoImplicitPrelude         #-}
+{-# LANGUAGE OverloadedStrings         #-}
+
+import           BasicPrelude      hiding (fromString)
+import qualified Data.Text         as T
+import qualified Data.Text.IO      as TIO
+import           System.Directory  (getCurrentDirectory)
+import           System.IO         (Handle (), IOMode (ReadMode), openFile,
+                                    stdin)
+import           Lib
+import           Test.Hspec
+import           Text.XML.HXT.Core
+
+openFixture :: forall s b. FilePath -> IO (IOStateArrow s b XmlTree)
+openFixture path = do
+    dir <- getCurrentDirectory
+    openXMLFile $ dir </> "test" </> "fixtures" </> path
+
+openXMLFile :: forall s b. FilePath -> IO (IOStateArrow s b XmlTree)
+openXMLFile = (readXMLFileHandle =<<) . getHandle
+  where
+    getHandle path = openFile path ReadMode
+
+readXMLFileHandle :: forall s b. Handle -> IO (IOStateArrow s b XmlTree)
+readXMLFileHandle h = do
+    contents <- TIO.hGetContents h
+    return $ readString [withWarnings yes] $ T.unpack contents
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $
+  describe "XML Parser" $
+    it "reads descriptions" $ do
+    file <- liftIO $ openFixture "sounds.rss"
+    descs <- liftIO $ readDescriptions file
+
+    length descs `shouldBe` 2
